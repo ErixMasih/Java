b04760a91df972feea4b7c466ad7c125d011bc44
Erix <139246964+ErixMasih@users.noreply.github.com>
2026-03-28 23:17:59 +0530

    Add files via upload

---
+++ Strings/Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Reverse {
+    public static void main(String args[]) {
+        String s = "Java is very easy";
+        int count = 1;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ')
+                count++;
+        }
+
+        System.out.println("Words: " + count);
+    }
+}
