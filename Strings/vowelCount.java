0ba3eb2d38c9d6cddcaf1eab2e4f10edef1ff15c
Erix <139246964+ErixMasih@users.noreply.github.com>
2026-03-28 22:43:01 +0530

    Add files via upload

---
+++ Strings/vowelCount.java
@@ -0,0 +1,19 @@
+package Java.Strings;
+
+import java.util.*;
+public class vowelCount {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter :");
+        String s=sc.nextLine();
+        int count=0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+                  if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                count++;
+        }
+          System.out.println("Vowels: " + count);
+    }
+}
