aacc738e7767957e45225f469fe4623124aa5415
Erix <139246964+ErixMasih@users.noreply.github.com>
2026-03-30 20:23:44 +0530

    Add files via upload

---
+++ Control Flow/Basic_if_else.java
@@ -0,0 +1,64 @@
+package Control Flow;
+
+public class Basic_if_else {
+    public static void main(String[] args) {
+//Basic        
+//          if (condition) {
+//     // runs when condition is true
+// } else {
+//     // runs when condition is false
+// }
+
+//if-elseif-else
+// int num = 7;
+// if (num % 2 == 0) {
+//     System.out.println("Even");
+// } else {
+//     System.out.println("Odd");
+// }
+// }
+
+
+// if-else if-else (ladder)
+// int marks = 75;
+
+// if (marks >= 90) {
+//     System.out.println("Grade: A");
+// } else if (marks >= 75) {
+//     System.out.println("Grade: B");
+// } else if (marks >= 60) {
+//     System.out.println("Grade: C");
+// } else {
+//     System.out.println("Grade: F");
+// }
+
+// Nested if-else
+// int a = 5, b = 10, c = 3;
+
+// if (a > b) {
+//     if (a > c) System.out.println("a is greatest");
+//     else        System.out.println("c is greatest");
+// } else {
+//     if (b > c) System.out.println("b is greatest");
+//     else        System.out.println("c is greatest");
+// }
+
+// Ternary Operator (Shorthand If-Else)
+//  
+
+// Find max of two numbers
+// int max = (a > b) ? a : b;
+
+// // Absolute value
+// int abs = (n < 0) ? -n : n;
+
+// // Check range
+// if (x >= 1 && x <= 100) {
+//     System.out.println("In range");
+// }
+
+// // Multiple conditions
+// if (age >= 18 && hasID) {
+//     System.out.println("Entry allowed");
+// }
+}}
